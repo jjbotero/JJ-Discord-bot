4055132201357df86126f1ced61cf8a9ffac57b6
jjbotero <j_botero@mail.fhsu.edu>
2021-01-20 14:12:03 -0500

    commiting bot project

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const { prefix, token } = require("./config.json");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+
+  if (msg.content.startsWith(`${prefix}fonsi`)) {
+    msg.channel.send('https://cdn.discordapp.com/attachments/591464241488461824/800631183734079538/FONSI.jpg');
+  } else if (msg.content.startsWith(`${prefix}friday`)) {
+    msg.channel.send('https://www.youtube.com/watch?v=kfVsfOSbJY0');
+  }
+  
+});
+
+client.login(token);
