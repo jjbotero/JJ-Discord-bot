12cee14dc46f89f622fd0ca94a9dbefeecc6eae0
jjbotero <j_botero@mail.fhsu.edu>
2021-01-20 17:10:20 -0500

    dynamic commands

---
+++ commands/fonsi.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'fonsi',
+	description: 'Despacito fonsi',
+	execute(message, args) {
+		message.channel.send('https://cdn.discordapp.com/attachments/591464241488461824/800631183734079538/FONSI.jpg');
+	},
+};
---
+++ commands/friday.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'friday',
+	description: 'it\'s finally Friday.',
+	execute(message, args) {
+		message.channel.send('https://www.youtube.com/watch?v=kfVsfOSbJY0');
+	},
+};
