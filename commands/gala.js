9d08cb7aa01fd2a44e93071178d289ea2d2446e8
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-01-22 00:42:35 -0500

    Add files via upload

---
+++ commands/gala.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'gala',
+	description: 'best league plays',
+	execute(message, args) {
+		message.channel.send('https://discord.com/channels/591462813852106781/591464241488461824/802040848556359702');
+	},
+};
