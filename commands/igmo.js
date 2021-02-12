0d71c694fc3212ba5b2886d51d7f94edabacd308
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-02-11 23:54:31 -0500

    Update igmo.js

--- commands/igmo.js
+++ commands/igmo.js
@@ -3,6 +3,6 @@ module.exports = {
 	name: 'igmo',
 	description: 'tank',
 	execute(message, args) {
-    message.channel.send('blob:https://www.kapwing.com/4588f10a-6e7b-49a3-8240-3fdb1b795cbd');
+    message.channel.send('https://i.imgur.com/XePFQEl.jpg');
 	},
 };
