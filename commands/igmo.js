e4397c9f82c3b98eb33429a817c45270e8add72f
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-02-11 23:52:28 -0500

    Create igmo.js

---
+++ commands/igmo.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	name: 'igmo',
+	description: 'tank',
+	execute(message, args) {
+    message.channel.send('blob:https://www.kapwing.com/4588f10a-6e7b-49a3-8240-3fdb1b795cbd');
+	},
+};
