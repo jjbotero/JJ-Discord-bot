bd5623be2e43d81e8526aeca3cb44e0bbeb20fdc
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-02-18 00:37:01 -0500

    new commands

---
+++ commands/garen.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'garen',
+	description: 'true colors',
+	execute(message, args) {
+    message.channel.send('https://i.imgur.com/08pIiZA.png');
+	},
+};
---
+++ commands/yuki.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'yuki',
+	description: 'poggies',
+	execute(message, args) {
+    message.channel.send('poggies, amber alert');
+	},
+};
