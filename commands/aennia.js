811dbfff12ddfcd9966403912cedbeadb1b3bb2c
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-02-18 00:31:18 -0500

    Create aennia.js

---
+++ commands/aennia.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'aennia',
+	description: 'toxic',
+	execute(message, args) {
+    message.channel.send('https://media.discordapp.net/attachments/737543949148749834/811493064606351370/unknown.png');
+	},
+};
