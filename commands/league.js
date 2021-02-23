b58708df35167e365e76439cceb3dacfe98dafac
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-02-23 00:35:02 -0500

    Create league.js

---
+++ commands/league.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'league',
+	description: 'league collection',
+	execute(message, args) {
+    message.channel.send('https://cdn.discordapp.com/attachments/591464241488461824/813636325768953873/image0.jpg');
+	},
+};
