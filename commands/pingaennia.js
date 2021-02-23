71e69e9d611221e411e80d354ed86e511c9cc6a1
jjbotero <j_botero@mail.fhsu.edu>
2021-02-23 10:12:18 -0500

    Update pingaennia.js

--- commands/pingaennia.js
+++ commands/pingaennia.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'pingaennia',
 	description: 'she needs to be ping',
 	execute(message, args) {
-    message.channel.send('<@nred#0645>');
+    message.channel.send("Hello <@88439417282256896>");
 	},
 };
