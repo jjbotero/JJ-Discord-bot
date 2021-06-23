fbd9ee723a85b9d1d90f5f97d975d5a458ef4884
jjbotero <45729763+jjbotero@users.noreply.github.com>
2021-06-22 22:58:29 -0400

    Update mommymilker.js

--- commands/mommymilker.js
+++ commands/mommymilker.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'mommymilker',
 	description: 'this is not good',
 	execute(message, args) {
-    message.channel.send("https://cdn.discordapp.com/attachments/591464241488461824/857038772154662952/AMONGUS.PNG @87712618642432000");
+    message.channel.send("https://cdn.discordapp.com/attachments/591464241488461824/857038772154662952/AMONGUS.PNG <@87712618642432000>");
 	},
 };
